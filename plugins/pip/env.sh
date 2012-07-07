2a64258292aa54fd3c4658ea3df11071bc71a233
Shane da Silva <shane@dasilva.io>
2012-07-06 21:53:06 -0700

    Don't print STDERR when checking if `pip` exists

--- plugins/pip/env.sh
+++ plugins/pip/env.sh
@@ -1,7 +1,7 @@
 # Enable autocompletion for pip (only available for bash/zsh)
 shell=`current_shell`
 if [[ "$shell" =~ (ba|z)sh ]]; then
-  which pip > /dev/null && \
+  which pip >/dev/null 2>&1 && \
     eval `PIP_REQUIRE_VIRTUALENV=false pip completion --$shell`
 fi
 
