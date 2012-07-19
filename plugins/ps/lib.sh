646e9a44efe2920a2aa9e9502c071b20055ef48b
Shane da Silva <shane@dasilva.io>
2012-07-18 20:35:38 -0700

    Remove psgrep/pskill helpers
    Turns out utilities like pgrep and pkill already exist for this sort of
    thing.

--- plugins/ps/lib.sh
+++
@@ -1,9 +0,0 @@
-# Search for a process by name
-psgrep () {
-  ps aux | grep "$@" | grep -v grep
-}
-
-# Kills any process that matches a regexp passed to it
-pskill () {
-  psgrep "$@" | awk '{print $2}' | xargs kill
-}
