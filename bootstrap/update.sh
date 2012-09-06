a1d4d2101b5b5729bc5bf4c0c535e375a795a687
Shane da Silva <shane@dasilva.io>
2012-09-05 22:12:19 -0700

    Teach Dot updates to work in other directories

--- bootstrap/update.sh
+++ bootstrap/update.sh
@@ -2,32 +2,36 @@
 update_dot () {
   local updated=1
   local stashed=0;
+  local old_dir=`pwd`
+
+  cd "$DOTDIR"
+
   local cur_revision=`git rev-parse HEAD`
 
   git fetch origin
-  if [ "`git rev-parse origin/master`" == "$cur_revision" ]; then
-    echo "No updates"
-    return 1
-  fi
+  if [ "`git rev-parse origin/master`" != "$cur_revision" ]; then
+    if ! git diff-index --quiet HEAD; then
+      git stash && stashed=1;
+    fi
 
-  if ! git diff-index --quiet HEAD; then
-    git stash && stashed=1;
-  fi
+    if ! git rebase origin/master; then
+      echo "Unable to update Dot; local branch has diverged from remote"
+      updated=0
+    else
+      source "$DOTDIR/bootstrap/setup.sh"
+      setup install
+      updated=$?
+    fi
 
-  if ! git rebase origin/master; then
-    echo "Unable to update Dot; local branch has diverged from remote"
-    updated=0
+    # If any stashed changes don't apply, undo the action
+    if [ $stashed -eq 1 ] && ! git stash pop; then
+      git reset --hard "$cur_revision" && git stash pop
+      echo "Error applying local changes; reverting to original revision"
+    fi
   else
-    source "$DOTDIR/bootstrap/setup.sh"
-    setup install
-    updated=$?
+    echo "No updates"
   fi
 
-  # If any stashed changes don't apply, undo the action
-  if [ $stashed -eq 1 ] && ! git stash pop; then
-    git reset --hard "$cur_revision" && git stash pop
-    echo "Error applying local changes; reverting to original revision"
-  fi
-
+  cd "$old_dir"
   return $updated
 }
