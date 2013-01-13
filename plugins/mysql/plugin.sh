a7faa89b6166500e1243a1860604e2fb2817be36
Shane da Silva <shane@dasilva.io>
2013-01-13 01:50:19 -0800

    Enhance mysql prompt
    Update the prompt displayed by the mysql command-line client to include
    the username and database in a colour scheme similar to the shell
    prompt.

--- plugins/mysql/plugin.sh
+++ plugins/mysql/plugin.sh
@@ -1,2 +1,5 @@
-# Specify location of MySQL history file
+# Colorized prompt
+export MYSQL_PS1="${RESET}${FG[240]}\\u${FG[234]}@${FG[245]}\\d ${FG[33]}mysql⨠${RESET} "
+
+# Location of mysql client command history file
 export MYSQL_HISTFILE="$DOTLOGDIR/mysql-history"
