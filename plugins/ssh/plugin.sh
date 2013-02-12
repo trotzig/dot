b019ebcaba24170947ed8fee64a792bd1220ac44
Shane da Silva <shane@dasilva.io>
2013-02-11 16:22:54 -0800

    Add timeout to `ssh-reagent`
    There were rare cases where `ssh-reagent` would hang when trying to
    connect to an invalid agent socket.
    
    In order to prevent this, the `ssh-add -l` call is now invoked via a
    `timeout` helper script so that if a interval is exceeded the script
    will be killed.

--- plugins/ssh/plugin.sh
+++ plugins/ssh/plugin.sh
@@ -4,16 +4,29 @@ ssh () {
   command ssh "$@"
 }
 
-# Reinitialize env vars for ssh-agent; useful when TMUX loses it on disconnect
+# Reinitialize SSH_AUTH_SOCK environment variable to point to a working SSH
+# agent socket. One situation where the need for this occurs is when your
+# connection drops while in a tmux session. The session will have its
+# SSH_AUTH_SOCK variable pointing to the old agent socket, but a new one will
+# have been created when you reconnected. This script finds the new agent socket
+# and uses it as your agent by setting SSH_AUTH_SOCK.
 ssh-reagent () {
-  for agent in /tmp/ssh-*/agent.*; do
-    export SSH_AUTH_SOCK=$agent
-    if ssh-add -l &> /dev/null; then
-      echo Found working SSH Agent:
-      ssh-add -l
-      return
-    fi
-  done
+  if ls /tmp/ssh-*/agent.* &> /dev/null; then # If socket(s) exists
+    for agent in /tmp/ssh-*/agent.*; do
+      export SSH_AUTH_SOCK=$agent
+      echo "Trying $agent"
+      # In rare cases, running `ssh-add -l` will hang on an invalid agent
+      # socket, so we run it with the `timeout` script to kill it if it hangs.
+      if $DOTPLUGIN/timeout -t 1 ssh-add -l &> /dev/null; then
+        echo Found working SSH Agent:
+        ssh-add -l
+        return
+      else
+        echo "Agent $agent appears dead; removing"
+        rm -r $agent
+      fi
+    done
+  fi
   echo Cannot find SSH agent--maybe you should reconnect and forward it?
   return 1
 }
