ee374c09ea229a1a46b592a32f78e1c7b793f9c2
Shane da Silva <shane@dasilva.io>
2013-03-06 18:21:42 -0800

    Add newline in MySQL prompt to fix prompt width
    MySQL counts the characters used to do colour coding in the prompt
    width, preventing the command a user enters from taking up the full
    width of the terminal since MySQL thinks the prompt is wider than it
    actually is.
    
    Adding a newline avoids this entirely by resetting the line that the
    command appears on to not have any colours on it.

--- plugins/mysql/plugin.sh
+++ plugins/mysql/plugin.sh
@@ -1,5 +1,8 @@
-# Colorized prompt
-export MYSQL_PS1="${RESET}${FG[240]}\\u${FG[234]}@${FG[245]}\\d ${FG[33]}mysql⨠${RESET} "
+# Colorize prompt. We need a newline because MySQL counts the characters used to
+# do colour coding in the prompt width, preventing the command from taking up
+# the full width of the terminal since it thinks the prompt is wider than it
+# actually is. The newline avoids this entirely by resetting the line.
+export MYSQL_PS1="${RESET}${FG[240]}\\u${FG[234]}@${FG[245]}\\h ${FG[33]}\\d${RESET}\\n⨠ "
 
 # Location of mysql client command history file
 export MYSQL_HISTFILE="$DOTLOGDIR/mysql-history"
