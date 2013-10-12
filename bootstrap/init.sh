944b10571700437b50bc6b7eedfc5d708ea25581
Shane da Silva <shane@dasilva.io>
2013-10-12 16:54:20 -0700

    Revert "Optimize current_shell helper"
    This reverts commit 55074092d0a105ab7b287e031a1ea789bf19ed73.
    
    Turns out that this optimization was incorrect, since `$SHELL` need not
    be set to the current shell, but just the default startup shell.

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -11,7 +11,15 @@ export PATH="$HOME/bin:/usr/local/bin:$PATH"
 
 # Returns a string representing the shell running this script
 current_shell () {
-  echo `basename $SHELL`
+  local shell=sh
+
+  if [ -n "$BASH_VERSION" ]; then
+    shell=bash
+  elif [ -n "$ZSH_VERSION" ]; then
+    shell=zsh
+  fi
+
+  echo "$shell"
 }
 
 # Sets an alias safely, printing an error if alias is already in use.
