a0ef94a4b87f743958be74aeb6a3c3abf87c8b29
Shane da Silva <shane@dasilva.io>
2013-10-12 17:14:34 -0700

    Remove spelling suggestions from zsh
    This usually offered to correct arguments that didn't need correction
    (e.g. git aliases).
    
    Turn it off.

--- plugins/autocorrect/plugin.zsh
+++ plugins/autocorrect/plugin.zsh
@@ -1,7 +1,2 @@
-# Attempt to correct spelling of all arguments in a command
-setopt correct_all
-
-# Don't autocorrect for these utilities as it doesn't make sense to
-alias man='nocorrect man'
-alias mkdir='nocorrect mkdir'
-alias mv='nocorrect mv'
+# Don't check spelling of arguments, as this doesn't play well with aliases
+unsetopt correct_all
