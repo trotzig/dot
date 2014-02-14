5c63385daf62a7a2e165394b224ac291a6db42e9
Shane da Silva <shane@dasilva.io>
2014-02-14 09:51:45 -0800

    Ensure Slate enabled for assistive devices
    This actually didn't work since the `install` script ran after the
    `setup` script, so the `cask_installed` check would always return true,
    causing the code not to run.
    
    Instead, make sure we're on OS X and then just always run the SQL insert
    command.

--- plugins/slate/setup.sh
+++ plugins/slate/setup.sh
@@ -1,5 +1,5 @@
 install() {
-  if ! cask_installed slate; then
+  if [ `uname` = Darwin ]; then
     # Ensure Assistive Devices are enabled for Slate
     sudo sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db \
       "INSERT OR REPLACE INTO access VALUES('kTCCServiceAccessibility','com.slate.Slate',0,1,1,NULL);"
