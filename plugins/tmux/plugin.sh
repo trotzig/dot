315f59da67b6ba1c200de62ea3c0a87573a58780
Henric Trotzig <henric.trotzig@gmail.com>
2020-02-09 21:14:53 +0100

    Add some uncommited stuff
    I'm about to clone sds's dotfiles once more. Before I do that, make sure
    I've pushed the latest state of things.

--- plugins/tmux/plugin.sh
+++ plugins/tmux/plugin.sh
@@ -1,23 +1,25 @@
-alias t=tmux
+alias t=tmux_with_ssh
 
-tmux() {
+tmux_with_ssh() {
   # Symlink auth socket so that when we reconnect we can point to the newer
   # socket by just updating the symlink. Saves us from having to manually reset
   # SSH_AUTH_SOCK using `ssh-reagent` or similar.
   SOCK_SYMLINK=~/.ssh/tmux_ssh_auth_sock
   if [ -r "$SSH_AUTH_SOCK" ]; then
     rm -f $SOCK_SYMLINK && ln -s $SSH_AUTH_SOCK $SOCK_SYMLINK
-    tmux_command="env SSH_AUTH_SOCK=$SOCK_SYMLINK `which tmux`"
+    tmux_env="SSH_AUTH_SOCK=$SOCK_SYMLINK"
   else
-    tmux_command=`which tmux`
+    tmux_env=""
   fi
 
-
   if [ -z "$@" ]; then
+    session_name="$(basename $(pwd))"
+    session_name="${session_name//./-}" # Periods aren't allowed as of tmux1.9a
+
     # Attach to session with the current directory name if one exists,
     # otherwise automatically create a session with the current directory name
-    $tmux_command new -A -s "$(basename $(pwd))"
+    env $tmux_env "$(which tmux)" new -A -s $session_name
   else
-    $tmux_command "$@"
+    env $tmux_env "$(which tmux)" "$@"
   fi
 }
